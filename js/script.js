efd64a82e9910735a6fb138ca6406b600eb2fae8
DraeRamsey <andrea.jos.ramsey@gmail.com>
2020-08-03 16:43:26 -0700

    Add animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+var backFin =    $('#back-fin');
+var fishBox =    $('#fish-box');
+
+
+$(document).ready(function(){
+  function fishFloat()
+  {
+    fishBox.animate({top:'+=40'}, 1000);
+    fishBox.animate({top:'-=40'}, 1000,'linear',fishFloat);
+  }
+
+  fishFloat();
+
+   function fishBob()
+    {
+      backFin.css({height:200});
+      backFin.animate ({height: 175},1500, 'linear',fishBob);
+    }
+
+  fishBob();
+
+  });
